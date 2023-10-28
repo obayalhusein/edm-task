f47a8944876427c46b21d33f112a1b5cde823dd4
obayalhusein <obayalhusein@gmail.com>
2023-10-28 16:01:50 +0300

    remove token in logout action

--- src/redux/auth/actions.ts
+++ src/redux/auth/actions.ts
@@ -8,30 +8,31 @@ interface LoginActionPayload {
 }
 
 export const loginAction = (payload: LoginActionPayload) => async (dispatch: Dispatch<AnyAction>) => {
-
   dispatch({
     type: AUTH_CONSTANSTS.LOGIN_REQUEST,
   })
 
   try {
-   const res =  await apiService.post('/auth/local', payload)
-    localStorage.setItem('token', res.data.token)
+    const res =  await apiService.post('/auth/local', payload);
+    localStorage.setItem('token', res.data.token);
     dispatch({
       type: AUTH_CONSTANSTS.LOGIN_SUCCESS,
       payload: res.data.token
-    })
-
-
-  } catch (error) {
+    });
+  }
+  catch (error) {
     dispatch({
       type: AUTH_CONSTANSTS.LOGIN_FAILURE,
       payload: error
-    })
+    });
   }
-
 
 }
 
-export const LogoutAction = () => async (dispatch: Dispatch<AnyAction>) => {
-  return;
+export const logoutAction = () => (dispatch: Dispatch) => {
+  localStorage.removeItem('token');
+
+  dispatch({
+    type: AUTH_CONSTANSTS.LOGOUT_SUCCESS,
+  });
 };
