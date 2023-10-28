c159848aefb57c2fb9bf4244a291ed0ae8880674
obayalhusein <obayalhusein@gmail.com>
2023-10-29 01:15:47 +0300

    add align and noGutters props to UiContainer / add search field to dashboard

--- src/components/UiElements/UiContainer/index.tsx
+++ src/components/UiElements/UiContainer/index.tsx
@@ -4,11 +4,13 @@ import './style.scss';
 interface UiContainerProps {
     children: ReactNode;
     maxWidth?: string;
+    noGutters?: boolean;
+    align?: 'start' | 'end';
 }
 
-const UiContainer: React.FC<UiContainerProps> = ({ children, maxWidth }) => {
+const UiContainer: React.FC<UiContainerProps> = ({ children, maxWidth, noGutters, align }) => {
     return (
-        <div className="ui-container" style={{ maxWidth: maxWidth }}>
+        <div className={`ui-container ${align} ${noGutters ? 'no-gutters': ''}`} style={{ maxWidth: maxWidth }}>
             {children}
         </div>
     );
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,22 @@
 import DashboardLayout from "../../layouts/DashboardLayout";
 import ProductsTable from "../../components/ProductsTable";
 import UiContainer from "../../components/UiElements/UiContainer";
+import UiTextInput from "../../components/UiElements/UiTextInput";
 
 const DashboardHomePage: React.FC = () => {
     return (
         <DashboardLayout>
             <UiContainer>
                 <h1>Products</h1>
+                <UiContainer maxWidth="200px" align="end" noGutters>
+                    <UiTextInput
+                        name="search"
+                        label="Search..."
+                        type="search"
+                        value=""
+                        onChange={() => {}}
+                    />
+                </UiContainer>
                 <ProductsTable />
             </UiContainer>
         </DashboardLayout>
