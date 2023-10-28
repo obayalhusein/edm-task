22f99f280f18d8ed7420f227c197d12ae0e99412
obayalhusein <obayalhusein@gmail.com>
2023-10-29 01:24:39 +0300

    add create product button to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,12 +2,25 @@ import DashboardLayout from "../../layouts/DashboardLayout";
 import ProductsTable from "../../components/ProductsTable";
 import UiContainer from "../../components/UiElements/UiContainer";
 import UiTextInput from "../../components/UiElements/UiTextInput";
+import UiButton from "../../components/UiElements/UiButton";
 
 const DashboardHomePage: React.FC = () => {
     return (
         <DashboardLayout>
             <UiContainer>
-                <h1>Products</h1>
+                <div className="flex justify-between">
+                    <h1>
+                        Products
+                    </h1>
+
+                    <UiButton
+                        label="Add Product"
+                        type="button"
+                        color="primary"
+                        fill="outline"
+                        onClick={() => {}}
+                    />
+                </div>
                 <UiContainer maxWidth="200px" align="end" noGutters>
                     <UiTextInput
                         name="search"
